b245e1f0d85393fc7cb0a7a35d709e9644693b17
diegolopez2002 <143581969+diegolopez2002@users.noreply.github.com>
2023-09-16 23:06:55 -0400

    Create project2.py

---
+++ project2.py
@@ -0,0 +1,34 @@
+#Diego Lopez
+#Sept 14 2023
+
+import re
+
+
+def read_codons(file):
+
+
+    newfile = open(file, "r")
+
+    data = newfile.read()
+
+    datalist = data.split("\n")
+
+    newfile.close()
+
+
+
+
+
+def read_evals(file):
+    pass
+
+def encode(sequence):
+    pass
+
+
+def decode(sequence):
+    pass
+
+def operate(sequence, eval_name):
+    pass
+
