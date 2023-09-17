3aeb8a2200e1fbc840d14f586b22ae448deb7246
diegolopez2002 <143581969+diegolopez2002@users.noreply.github.com>
2023-09-17 00:06:52 -0400

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,44 +1,41 @@
-#Diego Lopez
-#Sept 14 2023
-
 import re
+from functools import reduce
 
+def read_codons(codon_file):
+  # This will open a file with a given path (codon_file)
+  file = open(codon_file)
 
-def read_codons(file):
-
-
+  
+  # Iterates through a file, storing each line in the line variable
+  for line in file:
+    # Insert code here
     regexpattern = r"^[A-Z][a-zA-Z]* : ([AGCU]*+{\d}[AGCU]+}, )*[AGCU]+{\d}?"
 
-    parts = []
+    match = re.match(regexpattern, line.strip())
 
-    with open(file, 'r') as file:
-            for line in file:
-                
-                match = re.match(regexpattern, line.strip())
-
-                if match:
-                    parts = line.strip().split(': ')
-                    name = parts[0]
-                    sequences = parts[1].split(', ')
-
-                    
-
-   
+    if match:
+        parts = line.strip().split(': ')
+        name = parts[0]
+        sequences = parts[1].split(', ')
+        
+    
 
 
 
-
-
-def read_evals(file):
-    pass
+def read_evals(eval_file):
+  # This will open a file with a given path (eval_file)
+  file = open(eval_file)
+  
+  # Iterates through a file, storing each line in the line variable
+  for line in file:
+    # Insert code here
+    raise Exception("Not Implemented")
 
 def encode(sequence):
-    pass
-
+  raise Exception("Not Implemented")
 
 def decode(sequence):
-    pass
+  raise Exception("Not Implemented")
 
-def operate(sequence, eval_name):
-    pass
-
+def operate(sequence,eval_name):
+  raise Exception("Not Implemented")
