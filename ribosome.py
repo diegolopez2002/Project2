15f18829117c94bc7ddb52e43d2f97a0165eac4b
diegolopez2002 <143581969+diegolopez2002@users.noreply.github.com>
2023-09-17 20:43:22 -0400

    Update ribosome.py

--- ribosome.py
+++ ribosome.py
@@ -1,20 +1,17 @@
 import re
 from functools import reduce
 
-
 def read_codons(codon_file):
-    codon_dict = {}
-    
-    file = open(file)
-    
-    for line in file:
-        line = line.strip()
-        if re.match(r'^[A-Z][a-zA-Z]*:\s[AUGC]+\d*(,\s[AUGC]+\d*)*$', line):
-            parts = line.split(':')
-            amino_acid = parts[0].strip()
-            sequences = [seq.strip() for seq in parts[1].split(',')]
-            codon_dict[amino_acid] = sequences
 
+    file = open(codon_file)
+
+    for line in file:
+      raise Exception("Not Implemented")
+
+      
+
+
+    
 def read_evals(eval_file):
   # This will open a file with a given path (eval_file)
   file = open(eval_file)
