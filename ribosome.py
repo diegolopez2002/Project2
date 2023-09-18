b4f958ae39f4fd6bc73417bb5787d6472b077528
diegolopez2002 <143581969+diegolopez2002@users.noreply.github.com>
2023-09-18 01:21:42 -0400

    Update ribosome.py

--- ribosome.py
+++ ribosome.py
@@ -1,9 +1,25 @@
 import re
 from functools import reduce
 
+
+def read_evals(eval_file):
+  # This will open a file with a given path (eval_file)
+  file = open(eval_file)
+  
+  # Iterates through a file, storing each line in the line variable
+  for line in file:
+    # Insert code here
+    raise Exception("Not Implemented")
+
+def operate(sequence,eval_name):
+  raise Exception("Not Implemented")
+
+
+
 def read_codons(codon_file):
+    global codon_map
     
-    codon_map = {}
+    codon_map.clear()  
 
     def expand_sequence(sequence):
         matches = re.findall(r'\{(\d+)\}', sequence)
@@ -32,37 +48,23 @@ def read_codons(codon_file):
                     valid_sequences.append(expanded_seq)
             
             if valid_sequences:
-                codon_map[amino_acid] = valid_sequences
+                # Choose the longest sequence for the amino acid
+                codon_map[amino_acid] = max(valid_sequences, key=len)
 
-    return codon_map
-
-
-    
-def read_evals(eval_file):
-  # This will open a file with a given path (eval_file)
-  file = open(eval_file)
-  
-  # Iterates through a file, storing each line in the line variable
-  for line in file:
-    # Insert code here
-    raise Exception("Not Implemented")
 
 def encode(sequence):
     global codon_map
     
     rna_sequence = ""
-    amino = sequence.split(" ")
+    amino_acids = sequence.split(" ")
 
-    for a in amino:
-        if a in codon_map:
-            rna_sequence += codon_map[a]
+    for amino_acid in amino_acids:
+        rna_sequence += codon_map.get(amino_acid, "")
 
     return rna_sequence
 
 def decode(sequence):
     global codon_map
-    
-    
     reverse_map = {v: k for k, v in codon_map.items()}
     sorted_sequences = sorted(reverse_map.keys(), key=len, reverse=True)
 
@@ -72,7 +74,7 @@ def decode(sequence):
     while index < len(sequence):
         foundm = False
         for seq in sorted_sequences:
-            if sequence[index:index+len(seq)] == seq:
+            if sequence.startswith(seq, index):
                 amino_acids.append(reverse_map[seq])
                 index += len(seq)
                 foundm = True
@@ -82,6 +84,28 @@ def decode(sequence):
             index += 1
 
     return ' '.join(amino_acids)
+
+
+def read_evals(eval_file):
+  # This will open a file with a given path (eval_file)
+  file = open(eval_file)
+  list2 = []
+  # Iterates through a file, storing each line in the line variable
+  for line in file:
+    # Insert code here
+    parts = line.strip().split(':')
+    if len(parts) == 2:
+                    
+        order_name = parts[0].strip()
+        order_details = parts[1].strip().split(', ')
+
+        if all(detail in ['L', 'R', 'PO', 'PR', 'I'] for detail in order_details):
+          # Check if order name is valid
+          if order_name.isalnum():
+            list2.append((order_name, order_details))
+
+
+    
 
 def operate(sequence,eval_name):
   raise Exception("Not Implemented")
