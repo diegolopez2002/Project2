beb3c5926197e3a24b3a32af25588de3358a4588
diegolopez2002 <143581969+diegolopez2002@users.noreply.github.com>
2023-09-18 18:07:50 -0400

    Update ribosome.py

--- ribosome.py
+++ ribosome.py
@@ -1,36 +1,75 @@
 import re
 from functools import reduce 
 
-
+codon_dict = {}
+eval_dict = {}
 
 def read_codons(codon_file):
-    dictionary = {}
+    global codon_dict
+    codon_dict.clear()
+
     with open(codon_file, 'r') as file:
         for line in file:
-            key, value = line.strip().split(':')
-            dictionary[key.strip()] = sequence(value.strip())
-    return dictionary
+            parts = line.strip().split(':')
+            if len(parts) != 2:
+                continue
+
+            amino_acid, sequences = parts
+            sequences = [sequence(s.strip()) for s in sequences.split(',')]
+            sequences = [s for s in sequences if set(s) <= {"A", "G", "U", "C"}]
+            sequences.sort(key=len, reverse=True)
+            
+            if sequences:
+                codon_dict[amino_acid] = sequences
+
+
 
 def sequence(seq):
     return re.sub(r'([A-Z])\{(\d+)\}', lambda m: m.group(1) * int(m.group(2)), seq)
 
-def encode(sequence, codon_dict):
-    return codon_dict.get(sequence)
-
-def decode(sequence, codon_dict):
-    for key, value in codon_dict.items():
-        if sequence == value:
-            return key
-    return None
-    
 def read_evals(eval_file):
+    global eval_dict
+    eval_dict.clear()
+
+    pattern = re.compile(r'^[a-zA-Z0-9]+: (L|R), (PO|PR|I)$')
+    with open(eval_file, 'r') as file:
+        for line in file.strip().split("\n"):
+            if pattern.match(line):
+                order, commands = line.split(':')
+                read_order, operation_order = commands.strip().split(', ')
+                eval_dict[order] = (read_order, operation_order)
+
+def encode(aminos):
+    rna_seq = ""
+    for amino in aminos.split():
+        sequences = codon_dict.get(amino)
+        if sequences:
+            rna_seq += sequences[0]  # taking the longest one
+    return rna_seq
+
+def decode(sequence):
+    amino_seq = ""
+    while sequence:
+        for amino, sequences in codon_dict.items():
+            for seq in sequences:
+                if sequence.startswith(seq):
+                    amino_seq += amino + " "
+                    sequence = sequence[len(seq):]
+                    break
+        else:
+            sequence = sequence[1:]
+    return amino_seq.strip()
+    
+#def read_evals(eval_file):
   # This will open a file with a given path (eval_file)
-  file = open(eval_file)
+  
+#file = open(eval_file)
   
   # Iterates through a file, storing each line in the line variable
-  for line in file:
+  #for line in file:
     # Insert code here
-    pass
+    
+    #pass
 
 
 def operate(sequence,eval_name):
