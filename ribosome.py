520e77ba457d2c213094b9815390f8477d37c55d
diegolopez2002 <143581969+diegolopez2002@users.noreply.github.com>
2023-09-18 17:14:36 -0400

    Update ribosome.py

--- ribosome.py
+++ ribosome.py
@@ -2,6 +2,39 @@ import re
 from functools import reduce 
 
 
+
+def read_codons(codon_file):
+
+    file = open(codon_file)
+
+    codon.clear()
+    for line in file:
+        pattern = re.compile('(^[A-Z][a-zA-Z]*): (([AUGC]|\{[0-9]+\})+)(, ([AUGC]|\{[0-9]+\})+)*$')
+        match = re.match(pattern)
+
+        if match:
+            codon[match.group(1)] = match.group(2)
+
+        else:
+            continue 
+        
+        
+
+
+def encode(sequence):
+    
+    for i in codon:
+        if sequence == i:
+            return codon[i]
+    
+
+def decode(sequence):
+   
+    for i in codon:
+        if sequence == codon[i]:
+            return i
+
+
     
 def read_evals(eval_file):
   # This will open a file with a given path (eval_file)
@@ -15,51 +48,4 @@ def read_evals(eval_file):
 
 def operate(sequence,eval_name):
   raise Exception("Not Implemented")
-
-
-
-codon_dict = {}
-
-def read_codons(codon_file):
     
-    global codon_dict
-    codon_dict = {}  # Clear the dictionary
-    with open(codon_file, 'r') as f:
-        for line in f:
-            match = re.match(r'^([A-Z][a-zA-Z]*): (.+)$', line.strip())
-            if match:
-                name = match.group(1)
-                seqs = match.group(2).split(', ')
-                for i, seq in enumerate(seqs):
-                    seqs[i] = re.sub(r'\{(\d+)\}', lambda x: x.group()[-2]*int(x.group(1)), seq)
-                codon_dict[name] = max(seqs, key=len)
-    return
-
-    
-def encode(sequence):
-    amino = sequence.split(' ')
-    seq = ""
-    for aa in amino:
-        if aa in codon_dict:
-            seq += codon_dict[aa]
-    return seq
-  
- 
-
-def decode(sequence):
-   
-    global codons_dict
-    result = []
-    i = 0
-    while i < len(sequence):
-        # Take the longest sequence first, so iterate in reverse order
-        max_length = min(7, len(sequence) - i)  # As maximum codon length observed is 7
-        for length in range(max_length, 0, -1):
-            codon = sequence[i:i+length]
-            if codon in codons_dict:
-                result.append(codons_dict[codon])
-                i += length
-                break
-        else: 
-            i += 1
-    return ' '.join(result)
